ed467871c34b72b42c1b43bfab081771f0b8c6e7
joelezz <joel.ezzahid@gmail.com>
2023-09-22 12:03:29 +0300

    new commit for app.py and script.js

--- static/script.js
+++ static/script.js
@@ -46,7 +46,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-        fetch('https://kaswu-botti.azurewebsites.net/predict', {
+        fetch('http://0.0.0.0/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
