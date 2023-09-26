67b464328aeab154dabeb339bb7426f4d6eaaff6
Joel Ezzahid <77654189+joelezz@users.noreply.github.com>
2023-09-26 13:48:32 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -10,19 +10,18 @@ class Chatbox {
         this.messages = [];
 
         this.toggleState(this.args.chatBox);
-
-
+        this.displayWelcomeMessage(this.args.chatBox); // Lisää tervetuloviesti heti luonnin yhteydessä
     }
 
     display() {
-        const {openButton, chatBox, sendButton} = this.args;
+        const { openButton, chatBox, sendButton } = this.args;
 
         openButton.addEventListener('click', () => this.toggleState(chatBox))
 
         sendButton.addEventListener('click', () => this.onSendButton(chatBox))
 
         const node = chatBox.querySelector('input');
-        node.addEventListener("keyup", ({key}) => {
+        node.addEventListener("keyup", ({ key }) => {
             if (key === "Enter") {
                 this.onSendButton(chatBox)
             }
@@ -33,7 +32,7 @@ class Chatbox {
         this.state = !this.state;
 
         // show or hide the box
-        if(this.state) {
+        if (this.state) {
             chatbox.classList.add('chatbox--active')
         } else {
             chatbox.classList.remove('chatbox--active')
@@ -46,26 +45,12 @@ class Chatbox {
         if (text1 === "") {
             return;
         }
-    
-        let msg = { name: "User", message: text1 }
-        this.messages.push(msg);
 
-            // Lisää kirjoitusanimaatio botin viestiin
-        let msg1 = {
-            name: "Bot",
-            message: "<span class='typing-animation'></span>"
-        };
+        let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-        this.updateChatText(chatbox);
-        textField.value = '';
-    
-        // Lisää automaattinen viesti botilta alkuun
-        let msg2 = {
-            name: "Bot",
-            message: "Hei, olen Maria OpenAI:n ChatGPT:n tehostama apuri. Autan sinua saavuttamaan tavoitteesi tällä voit aloittaa esimerkiksi täyttämällä: \'<a href=\"{google_forms}\">Tavoitelomake</a>\' tai voidaan jutella vielä lisää, mutta voit kysyä minulta aina lomakkeesta, niin lähetän sen sinulle. Voit tutustua myös <a href=\"https://openai.com/policies/privacy-policy\">OpenAI:n Tietosuojalomakkeeseen</a>"
-        };
-        this.messages.unshift(msg2);
+        // Display typing animation while the bot generates a response
+        this.displayBotTyping(chatbox);
 
         fetch('https://kaswu-botti.azurewebsites.net/predict', {
             method: 'POST',
@@ -76,44 +61,66 @@ class Chatbox {
             },
         })
             .then(r => r.json())
-
             .then(r => {
-                let msg3 = { name: "Bot", message: r.message };
-                    textField.value = ''
+                // Remove the typing animation
+                this.removeBotTyping(chatbox);
 
-            setTimeout(() => {
-                this.messages.pop(); // Poista kirjoitusanimaatio
-                this.updateChatText(chatbox);
-        
+                let msg3 = { name: "Bot", message: r.message };
                 this.messages.push(msg3);
-                this.updateChatText(chatbox);
-            }, 3000); // 3 sekunnin viive
-                
-    
-            })
-            
-            .catch((error) => {
-                console.error('Error:', error);
+
                 this.updateChatText(chatbox)
                 textField.value = ''
-            });
 
-            
+            }).catch((error) => {
+                console.error('Error:', error);
+
+                // Remove the typing animation
+                this.removeBotTyping(chatbox);
+
+                // Update the chatbox with an error message
+                this.updateChatText(chatbox);
+                textField.value = ''
+
+            });
+    }
+
+    // Function to display the bot is typing message
+    displayBotTyping(chatbox) {
+        const botTypingMessage = '<div class="typing-animation">...</div>';
+        const chatmessage = chatbox.querySelector('.chatbox__messages');
+        chatmessage.innerHTML += botTypingMessage;
+    }
+
+    // Function to remove the bot is typing message
+    removeBotTyping(chatbox) {
+        const chatmessage = chatbox.querySelector('.chatbox__messages');
+        const botTypingElement = chatmessage.querySelector('.typing-animation');
+        if (botTypingElement) {
+            chatmessage.removeChild(botTypingElement);
         }
-    
+    }
+
+    displayWelcomeMessage(chatbox) {
+        // Lisää tervetuloviesti
+        let welcomeMessage = {
+            name: "Bot",
+            message: "Hei, olen täällä auttamassa sinua saavuttamaan tavoitteesi niin työssä kuin vapaa-ajalla. Anna minulle tavoitteesi, ja ohjaan sinua kohti niiden saavuttamista muutamilla yhteydenotoilla ja henkilökohtaisella tuellani. Voit tutustua myös tietosuojaan <a href=\"https://openai.com/policies/privacy-policy\">OpenAI:n Tietosuojalomake</a>"
+        };
+
+        this.messages.push(welcomeMessage);
+        this.updateChatText(chatbox);
+    }
 
     updateChatText(chatbox) {
         var html = '';
-        this.messages.slice().reverse().forEach(function(item, _index) {
-            if (item.name === "Bot")
-            {   
+        this.messages.slice().reverse().forEach(function (item, _index) {
+            if (item.name === "Bot") {
                 html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
             }
-            else
-            {
+            else {
                 html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
             }
-          });
+        });
 
         const chatmessage = chatbox.querySelector('.chatbox__messages');
         chatmessage.innerHTML = html;
