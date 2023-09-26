6f192df9101b26322a3efbd67bac843291b20efd
Joel Ezzahid <77654189+joelezz@users.noreply.github.com>
2023-09-26 13:53:59 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -10,18 +10,18 @@ class Chatbox {
         this.messages = [];
 
         this.toggleState(this.args.chatBox);
-        this.displayWelcomeMessage(this.args.chatBox); // Lisää tervetuloviesti heti luonnin yhteydessä
+
     }
 
     display() {
-        const { openButton, chatBox, sendButton } = this.args;
+        const {openButton, chatBox, sendButton} = this.args;
 
         openButton.addEventListener('click', () => this.toggleState(chatBox))
 
         sendButton.addEventListener('click', () => this.onSendButton(chatBox))
 
         const node = chatBox.querySelector('input');
-        node.addEventListener("keyup", ({ key }) => {
+        node.addEventListener("keyup", ({key}) => {
             if (key === "Enter") {
                 this.onSendButton(chatBox)
             }
@@ -32,7 +32,7 @@ class Chatbox {
         this.state = !this.state;
 
         // show or hide the box
-        if (this.state) {
+        if(this.state) {
             chatbox.classList.add('chatbox--active')
         } else {
             chatbox.classList.remove('chatbox--active')
@@ -45,13 +45,17 @@ class Chatbox {
         if (text1 === "") {
             return;
         }
-
+    
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
-
-        // Display typing animation while the bot generates a response
-        this.displayBotTyping(chatbox);
-
+    
+        // Lisää automaattinen viesti botilta alkuun
+        let msg2 = {
+            name: "Bot",
+            message: "Hei, olen Maria OpenAI:n ChatGPT:n voimauttama apuri. Autan sinua saavuttamaan tavoitteesi tällä voit aloittaa esimerkiksi täyttämällä: \'<a href=\"{google_forms}\">Tavoitelomake</a>\' tai voidaan jutella vielä lisää, mutta voit kysyä minulta aina lomakkeesta, niin lähetän sen sinulle. Voit tutustua myös <a href=\"https://openai.com/policies/privacy-policy\">OpenAI:n Tietosuojalomakkeeseen</a>"
+        };
+        this.messages.unshift(msg2);
+    
         fetch('https://kaswu-botti.azurewebsites.net/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
@@ -62,65 +66,32 @@ class Chatbox {
         })
             .then(r => r.json())
             .then(r => {
-                // Remove the typing animation
-                this.removeBotTyping(chatbox);
-
                 let msg3 = { name: "Bot", message: r.message };
                 this.messages.push(msg3);
-
+    
                 this.updateChatText(chatbox)
                 textField.value = ''
-
+    
             }).catch((error) => {
                 console.error('Error:', error);
-
-                // Remove the typing animation
-                this.removeBotTyping(chatbox);
-
-                // Update the chatbox with an error message
-                this.updateChatText(chatbox);
+                this.updateChatText(chatbox)
                 textField.value = ''
-
             });
     }
-
-    // Function to display the bot is typing message
-    displayBotTyping(chatbox) {
-        const botTypingMessage = '<div class="typing-animation">...</div>';
-        const chatmessage = chatbox.querySelector('.chatbox__messages');
-        chatmessage.innerHTML += botTypingMessage;
-    }
-
-    // Function to remove the bot is typing message
-    removeBotTyping(chatbox) {
-        const chatmessage = chatbox.querySelector('.chatbox__messages');
-        const botTypingElement = chatmessage.querySelector('.typing-animation');
-        if (botTypingElement) {
-            chatmessage.removeChild(botTypingElement);
-        }
-    }
-
-    displayWelcomeMessage(chatbox) {
-        // Lisää tervetuloviesti
-        let welcomeMessage = {
-            name: "Bot",
-            message: "Hei, olen täällä auttamassa sinua saavuttamaan tavoitteesi niin työssä kuin vapaa-ajalla. Anna minulle tavoitteesi, ja ohjaan sinua kohti niiden saavuttamista muutamilla yhteydenotoilla ja henkilökohtaisella tuellani. Voit tutustua myös tietosuojaan <a href=\"https://openai.com/policies/privacy-policy\">OpenAI:n Tietosuojalomake</a>"
-        };
-
-        this.messages.push(welcomeMessage);
-        this.updateChatText(chatbox);
-    }
+    
 
     updateChatText(chatbox) {
         var html = '';
-        this.messages.slice().reverse().forEach(function (item, _index) {
-            if (item.name === "Bot") {
+        this.messages.slice().reverse().forEach(function(item, _index) {
+            if (item.name === "Bot")
+            {   
                 html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
             }
-            else {
+            else
+            {
                 html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
             }
-        });
+          });
 
         const chatmessage = chatbox.querySelector('.chatbox__messages');
         chatmessage.innerHTML = html;
