fa216903e3fa429455ee5a9783b30372c3f125c5
joelezz <joel.ezzahid@gmail.com>
2023-09-30 10:52:37 +0300

    new version

--- static/script.js
+++ static/script.js
@@ -110,7 +110,7 @@ class Chatbox {
         this.addTypingAnimation(chatbox);
 
     
-        fetch('http://127.0.0.1:5000/predict', {
+        fetch('https://kaswu-botti.azurewebsites.net/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
@@ -149,40 +149,7 @@ class Chatbox {
         const chatmessage = chatbox.querySelector('.chatbox__messages');
         chatmessage.innerHTML = html;
 
-        const snippets = document.querySelectorAll('.snippet');
-
-for (let i = 0; i < snippets.length; i++) {
-  snippets[i].addEventListener('mouseleave', clearTooltip);
-  snippets[i].addEventListener('blur', clearTooltip);
-}
-
-function showTooltip(el, msg) {
-  el.setAttribute('class', 'snippet tooltip');
-  el.setAttribute('aria-label', msg);
-}
-
-function clearTooltip(e) {
-  e.currentTarget.setAttribute('class', 'snippet');
-  e.currentTarget.removeAttribute('aria-label');
-}
-
-const clipboardSnippets = new ClipboardJS('.snippet', {
-  text: trigger => trigger.getAttribute('data-title')
-});
-
-clipboardSnippets.on('success', e => {
-  e.clearSelection();
-  showTooltip(e.trigger, 'Copied!');
-});
-
-clipboardSnippets.on('error', e => {
-  showTooltip(e.trigger, 'Copy failed!');
-});
-
     }
 }
-
-
-
 
 const chatbox = new Chatbox();
