54e2280e221d6dc806b6d26401571bff555501da
joelezz <joel.ezzahid@gmail.com>
2023-10-02 20:33:00 +0300

    new commit

--- static/script.js
+++ static/script.js
@@ -110,7 +110,7 @@ class Chatbox {
         this.addTypingAnimation(chatbox);
 
     
-        fetch('https://mariagpt.azurewebsites.net:8000/predict', {
+        fetch('https://mariagpt.azurewebsites.net/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
