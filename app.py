b994783d2281408a573266e53d8da18711b8ab4d
joelezz <joel.ezzahid@gmail.com>
2023-10-02 20:23:37 +0300

    cors added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import openai
 import os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
+CORS(app)
 
 load_dotenv()
 google_forms = os.getenv("GOOGLE_FORMS")
